de358e37e0bb3de8f975e2e93973957a764829b0
CavLuni <denis.luniere@gmail.com>
2020-05-04 13:05:16 -0300

    Classe Categoria

--- src/main/java/com/cavluni/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/cavluni/cursomc/resource/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.cavluni.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cavluni.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar () {
-		return "REST está funcionando";
+	public List<Categoria> listar () {
+		
+		Categoria cat = new Categoria (1, "Informática");
+		Categoria cat2 = new Categoria (2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
