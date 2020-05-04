fdb7f2ab5852f54ceb001eca6e09ab6c933bff35
CavLuni <denis.luniere@gmail.com>
2020-05-04 18:15:48 -0300

    Implementando Cidade e Estado

---
+++ src/main/java/com/cavluni/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.cavluni.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cavluni.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
