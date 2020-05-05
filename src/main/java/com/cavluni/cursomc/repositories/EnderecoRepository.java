f7e172ca6711efb572e15ae70f949395122a8dc6
CavLuni <denis.luniere@gmail.com>
2020-05-05 13:03:21 -0300

    Cliente, telefones e endereços

---
+++ src/main/java/com/cavluni/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.cavluni.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cavluni.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
