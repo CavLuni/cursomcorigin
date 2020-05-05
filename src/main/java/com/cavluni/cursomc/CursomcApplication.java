19a2f640eb8c574b12b2baa7c209cdb9cdcee638
CavLuni <denis.luniere@gmail.com>
2020-05-05 16:58:20 -0300

    pedido EstadoPagamento e Pagamento

--- src/main/java/com/cavluni/cursomc/CursomcApplication.java
+++ src/main/java/com/cavluni/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.cavluni.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.cavluni.cursomc.domain.Cidade;
 import com.cavluni.cursomc.domain.Cliente;
 import com.cavluni.cursomc.domain.Endereco;
 import com.cavluni.cursomc.domain.Estado;
+import com.cavluni.cursomc.domain.Pagamento;
+import com.cavluni.cursomc.domain.PagamentoComBoleto;
+import com.cavluni.cursomc.domain.PagamentoComCartao;
+import com.cavluni.cursomc.domain.Pedido;
 import com.cavluni.cursomc.domain.Produto;
+import com.cavluni.cursomc.domain.enuns.EstadoPagamento;
 import com.cavluni.cursomc.domain.enuns.TipoCliente;
 import com.cavluni.cursomc.repositories.CategoriaRepository;
 import com.cavluni.cursomc.repositories.CidadeRepository;
 import com.cavluni.cursomc.repositories.ClienteRepository;
 import com.cavluni.cursomc.repositories.EnderecoRepository;
 import com.cavluni.cursomc.repositories.EstadoRepository;
+import com.cavluni.cursomc.repositories.PagamentoRepository;
+import com.cavluni.cursomc.repositories.PedidoRepository;
 import com.cavluni.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +97,22 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 		
 	}
---
+++ src/main/java/com/cavluni/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.cavluni.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cavluni.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
