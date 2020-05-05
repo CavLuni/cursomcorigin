b419201503331c37d08c532b58ea44375a8743ec
CavLuni <denis.luniere@gmail.com>
2020-05-05 18:35:28 -0300

    Item pedido Item pedido PK

--- src/main/java/com/cavluni/cursomc/CursomcApplication.java
+++ src/main/java/com/cavluni/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.cavluni.cursomc.domain.Cidade;
 import com.cavluni.cursomc.domain.Cliente;
 import com.cavluni.cursomc.domain.Endereco;
 import com.cavluni.cursomc.domain.Estado;
+import com.cavluni.cursomc.domain.ItemPedido;
 import com.cavluni.cursomc.domain.Pagamento;
 import com.cavluni.cursomc.domain.PagamentoComBoleto;
 import com.cavluni.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.cavluni.cursomc.repositories.CidadeRepository;
 import com.cavluni.cursomc.repositories.ClienteRepository;
 import com.cavluni.cursomc.repositories.EnderecoRepository;
 import com.cavluni.cursomc.repositories.EstadoRepository;
+import com.cavluni.cursomc.repositories.ItemPedidoRepository;
 import com.cavluni.cursomc.repositories.PagamentoRepository;
 import com.cavluni.cursomc.repositories.PedidoRepository;
 import com.cavluni.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -113,6 +117,19 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 		
 	}
---
+++ src/main/java/com/cavluni/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.cavluni.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cavluni.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
