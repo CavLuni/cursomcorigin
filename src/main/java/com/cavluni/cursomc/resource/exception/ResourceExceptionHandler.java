fce5e752b73d0a05418162ebebb3a3aabd8ff432
CavLuni <denis.luniere@gmail.com>
2020-05-06 12:44:29 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/cavluni/cursomc/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/cavluni/cursomc/resource/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cavluni.cursomc.services.exceptions.DataIntegretyException;
 import com.cavluni.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -17,6 +18,13 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegretyException.class)
+	public ResponseEntity<StandardError> dataIntegrety(DataIntegretyException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
 }
--- src/main/java/com/cavluni/cursomc/services/CategoriaService.java
+++ src/main/java/com/cavluni/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.cavluni.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.cavluni.cursomc.domain.Categoria;
 import com.cavluni.cursomc.repositories.CategoriaRepository;
+import com.cavluni.cursomc.services.exceptions.DataIntegretyException;
 import com.cavluni.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegretyException("Não é possível excluir uma categoria que possui produtos!");
+		}
+	
+	}
 	
 }
