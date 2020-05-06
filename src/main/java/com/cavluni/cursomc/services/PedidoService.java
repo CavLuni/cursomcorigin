6e7d060ab791160414c53eca01acc8efa0269ee1
CavLuni <denis.luniere@gmail.com>
2020-05-06 11:28:10 -0300

    Endpoint pedidos

---
+++ src/main/java/com/cavluni/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.cavluni.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cavluni.cursomc.domain.Pedido;
+import com.cavluni.cursomc.repositories.PedidoRepository;
+import com.cavluni.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+	}
+
+	
+}
