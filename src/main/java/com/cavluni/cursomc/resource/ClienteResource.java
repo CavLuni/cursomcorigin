488e77d00593417fa9887b847b1f0e6f1092d6ac
CavLuni <denis.luniere@gmail.com>
2020-05-08 12:49:01 -0300

    inserindo novo cliente com POST

--- src/main/java/com/cavluni/cursomc/resource/ClienteResource.java
+++ src/main/java/com/cavluni/cursomc/resource/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cavluni.cursomc.domain.Cliente;
 import com.cavluni.cursomc.dto.ClienteDTO;
+import com.cavluni.cursomc.dto.ClienteNewDTO;
 import com.cavluni.cursomc.services.ClienteService;
 
 @RestController
@@ -34,13 +35,13 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
-	/*@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto) {
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
 		Cliente obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-	}*/
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
