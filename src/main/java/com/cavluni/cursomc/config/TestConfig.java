ec7ddd2e80794c9f5f282b1fbdf2eaeb7559c444
CavLuni <denis.luniere@gmail.com>
2020-05-13 17:24:33 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/cavluni/cursomc/config/TestConfig.java
+++ src/main/java/com/cavluni/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.cavluni.cursomc.services.DBService;
+import com.cavluni.cursomc.services.EmailService;
+import com.cavluni.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -20,5 +22,10 @@ public class TestConfig {
 	public boolean instantiateDataBase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
---
+++ src/main/java/com/cavluni/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.cavluni.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.cavluni.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+
+}
