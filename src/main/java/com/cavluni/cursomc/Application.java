01d20f8d3574fc625daa68ee3d41a04af991efff
CavLuni <denis.luniere@gmail.com>
2020-05-22 17:12:51 -0300

    Salvando primeiro arquivo S3 no Amazon

---
+++ src/main/java/com/cavluni/cursomc/Application.java
@@ -0,0 +1,26 @@
+package com.cavluni.cursomc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cavluni.cursomc.services.S3Service;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner{
+
+	
+	@Autowired
+	public S3Service s3Service;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\Users\\denis\\OneDrive\\Desktop\\Programação\\PicturesTestes\\TestAna.jpg");
+	}	
+		
+}
