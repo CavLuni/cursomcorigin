ca2fc25723cfc2610f5b3ad9ab99d5e53c446c6a
CavLuni <denis.luniere@gmail.com>
2020-05-26 16:56:37 -0300

    Expondo o header location nas respostas

--- src/main/java/com/cavluni/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/cavluni/cursomc/security/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.cavluni.cursomc.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.cavluni.cursomc.dto.CredenciaisDTO;
@@ -56,7 +58,8 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         res.addHeader("Authorization", "Bearer " + token);
         res.addHeader("access-control-expose-headers", "Authorization"); 
 	}
-/*
+
+	@SuppressWarnings("unused")
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
         @Override
@@ -75,5 +78,5 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 + "\"message\": \"Email ou senha inválidos\", "
                 + "\"path\": \"/login\"}";
         }
-    }*/
+    }
 }
